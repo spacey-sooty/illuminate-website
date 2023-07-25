8a4064fb40255cb053ce94bbdc69b147e893863d
Isaac Turner <spacey_sooty@outlook.com>
2023-07-25 12:27:18 +0800

    basic work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ import styles from '@/styles/pages/home.module.scss'
 export default function Home() {
   return (
     <main>
-   </main>
+      <div>
+        <h1> StationAIry! </h1>
+        <p> StationAIry are the creators of the MechaPen a high quality pen which warns you when
+        you are running low on ink! </p>
+      </div>
+    </main>
   )
 }
