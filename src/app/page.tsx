f7d7e796cf412e3f5d7fa9676b1ebeb5f270efd5
Isaac Turner <spacey_sooty@outlook.com>
2023-07-28 10:16:34 +0800

    added content to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import styles from '@/styles/pages/home.module.scss'
 import Image from 'next/image'
 
 export default function Home() {
-  var h = 81.859 * 5;
-  var w = 10.732 * 5;
   return (
     <main>
       <div>
         <h1> StationAIry ! </h1>
-        <p className='hero'> StationAIry are the creators of the MechaPen a high quality pen which warns you when you are running low on ink! </p>
-      </div>
+        <p className='hero'> StationAIry is a WA based company focused on producing high quality stationary to reduce stress in students! </p>
+        <p className='hero'> Everyone has been there, you&apos;re going into an exam, you are only able to bring one pen, you&apos;re stressed. 
+        Will your pen have enough ink to get you through? You could just buy a new pen but that isn&apos;t sustainable! But this isn&apos;t 
+        a worry with the MechaPen! The  MechaPen warns you how much ink you have left and tells you how long you have left writing. </p>
+    </div>
     </main>
   )
 }
